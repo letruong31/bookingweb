68b868cffaee0e9c7abaec52c3bc6986f9e8c5b7
cvukxwhoo <chienvuk@gmail.com>
2023-07-18 23:22:28 +0700

    slider and header update

--- home/main.js
+++ home/main.js
@@ -1,21 +1,40 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+window.addEventListener("load", function() {
+  const slider = document.querySelector("#slider")
+  const sliderMain = document.querySelector(".slider-main")
+  const sliderItems = document.querySelectorAll(".slider-item")
+  const nextBtn = document.querySelector(".slider-next")
+  const prevBtn = document.querySelector(".slider-prev")
+  const sliderItemWidth = sliderItems[0].offsetWidth;
+  const slidesLength = sliderItems.length;
+  let positionX = 0;
+  let index = 0
 
-function plusDivs(n) {
-  showDivs((slideIndex += n));
-}
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("slider-img");
-  if (n > x.length) {
-    slideIndex = 1;
+  nextBtn.addEventListener("click", function() {
+    changeSlide(1);
+  })
+
+  prevBtn.addEventListener("click", function() {
+    changeSlide(-1);
+  })
+
+  function changeSlide(direction) {
+    if(direction === 1) {
+      if(index >= slidesLength - 1) {
+        index = slidesLength -1;
+        return;
+      }
+      positionX = positionX - sliderItemWidth;
+      sliderMain.style = `transform: translateX(${positionX}px)`
+      index++;
+    } else if (direction === -1 && direction !== 0) {
+      if(index <= 0) {
+        index = 0;
+        return;
+      }
+      positionX = positionX + sliderItemWidth;
+      sliderMain.style = `transform: translateX(${positionX}px)`
+      index--;
+    }
   }
-  if (n < 1) {
-    slideIndex = x.length;
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex - 1].style.display = "block";
-}
+})
