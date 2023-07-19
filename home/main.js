a44bf13904129e3ec0c551601274b98609a5acef
cvukxwhoo <chienvuk@gmail.com>
2023-07-20 00:59:44 +0700

    finish header and slider

--- home/main.js
+++ home/main.js
@@ -1,3 +1,4 @@
+// slider move
 window.addEventListener("load", function() {
   const slider = document.querySelector("#slider")
   const sliderMain = document.querySelector(".slider-main")
@@ -38,3 +39,31 @@ window.addEventListener("load", function() {
     }
   }
 })
+
+// Date at .form-item
+window.onload=function(){
+  populatedropdown("daydropdown", "monthdropdown", "yeardropdown")
+  }
+  var monthtext = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+function populatedropdown(dayfield, monthfield, yearfield) {
+  var today = new Date()
+  var dayfield = document.getElementById(dayfield)
+  var monthfield = document.getElementById(monthfield)
+  var yearfield = document.getElementById(yearfield)
+  for (var i = 1; i < 32; i++)
+    dayfield.options[i] = new Option(i, i)
+  //select today's day
+  dayfield.options[today.getDate()] = new Option(today.getDate(), today.getDate(), true, true)
+  for (var m = 0; m < 12; m++)
+    monthfield.options[m] = new Option(monthtext[m], monthtext[m])
+  //select today's month
+  monthfield.options[today.getMonth()] = new Option(monthtext[today.getMonth()], monthtext[today.getMonth()], true, true)
+  var thisyear = today.getFullYear()
+  for (var y = 0; y < 118; y++) {
+    yearfield.options[y] = new Option(thisyear, thisyear)
+    thisyear -= 1
+  }
+  //select today's year
+  yearfield.options[0] = new Option(today.getFullYear(), today.getFullYear(), true, true)
+}
